20f6177eacbe7dd8918b078b1c3d9336f14332ba
Krishna-Sai <saikrishnagokam@gmail.com>
2025-07-09 12:06:42 +0530

    Update AuthModal.jsx

--- client/src/components/AuthModal.jsx
+++ client/src/components/AuthModal.jsx
@@ -11,7 +11,7 @@ import {
 import { motion } from "framer-motion";
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
 const AuthModal = ({ isOpen, onClose, initialMode, onLogin }) => {
   const location = useLocation();
